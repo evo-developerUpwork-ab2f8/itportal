16843d3af1c7107611684f1c73971be1561af5fd
khalidmaghawry96 <khalidmaghawry96@icloud.com>
2017-11-13 17:57:19 +0200

    changes for issue_6

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -6,8 +6,28 @@ exports.inventoryhomePage = (req, res) => {
 };
 
 exports.addSoftwareLicence = (req, res, next) => {
-  const { name, manufacturer, version } = req.body;
-  SoftwareLicence.create({name, manufacturer, version})
+  const {
+    software_name, manufacturer,
+    product_key, seats,
+    company, licenced_to_name,
+    licenced_to_email, reassignable,
+    supplier, order_number, purchase_cost,
+    purchase_date, expiration_date,
+    termination_date, purchase_order_number,
+    depreciation, maintained, notes
+  } = req.body;
+
+  SoftwareLicence.create({
+    software_name, manufacturer,
+    product_key, seats,
+    company, licenced_to_name,
+    licenced_to_email, reassignable,
+    supplier, order_number,
+    purchase_cost, purchase_date,
+    expiration_date, termination_date,
+    purchase_order_number,
+    depreciation, maintained, notes
+  })
     .then((licence) => res.redirect(`/inventory/softwarelicence/${licence._id}`))
     .catch(err => next(err));
 };
--- models/SoftwareLicence.js
+++ models/SoftwareLicence.js
@@ -2,17 +2,59 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const licenceSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Name Must Be Provided']
+  software_name: {
+    type: String
   },
   manufacturer: {
-    type: String,
-    required: [true, 'Manufacturer Must Be Provided']
+    type: String
   },
-  version: {
-    type: String,
-    required: [true, 'Version Must Be Provided']
+  product_key: {
+    type: String
+  },
+  seats: {
+    type: String
+  },
+  company: {
+    type: String
+  },
+  licenced_to_name: {
+    type: String
+  },
+  licenced_to_email: {
+    type: String
+  },
+  reassignable: {
+    type: Boolean
+  },
+  supplier: {
+    type: String
+  },
+  order_number: {
+    type: Number
+  },
+  purchase_cost: {
+    type: Number
+  },
+  purchase_date: {
+    type: Date
+  },
+  expiration_date: {
+    type: Date
+  },
+  termination_date: {
+    type: Date
+  },
+  purchase_order_number: {
+    type: Number
+  },
+  depreciation: {
+    type: String
+  },
+  maintained: {
+    type: Boolean
+  },
+  notes: {
+    type: String
   }
 });
 
